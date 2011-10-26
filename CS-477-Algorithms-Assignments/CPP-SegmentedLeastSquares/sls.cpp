61594755bcf891295af55d290204c7abbcf1a5ba
= <=>
2011-10-25 23:10:26 -0700

    here we gooo..

---
+++ CS-477-Algorithms-Assignments/CPP-SegmentedLeastSquares/sls.cpp
@@ -0,0 +1,19 @@
+// Matt Geyer
+// CS 477 Algorithms HW 5
+// Segmented Least Squares
+//
+// Do segmented least squares using dynamic programming
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main( int argc , char** argv ){
+
+
+
+
+    return 0;
+}
